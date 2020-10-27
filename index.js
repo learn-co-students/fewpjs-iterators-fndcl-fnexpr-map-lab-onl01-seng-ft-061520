4a1029fdc40ac9885972e11d47f177fc5babb438
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-10-27 14:53:59 -0600

    Done.

--- index.js
+++ index.js
@@ -18,11 +18,19 @@ const tutorials = [
 const titleCased = () =>
     //Sperate each words
     tutorials.map(tutorial =>
-
         //get each letter in the words.. and map over each letter in the word
         tutorial.split(" ").map(
             //Capilazie the First letter in those words and Slice each word from the 1st letter in that word
             word => word[0].toUpperCase() + word.slice(1)
         ).join(' ') // The Join it all back together 
-
     )
+
+// const titleCased = (input) => {
+//     return tutorials.map( line => {
+//         let tokens = line.split(' ')
+//         let capitalizedTokens =
+//         tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) )
+//         let response = capitalizedTokens.join(' ')
+//         return response
+//     })
+// }
